2d92b99347d6bbac553fa1538dff332bbeba6dd3
LanzPovey <40040682+LanzPovey@users.noreply.github.com>
2022-08-05 20:01:44 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Lanz Povey
+Console.WriteLine("Test1");
+Console.WriteLine("Test2");
+Console.WriteLine("Test3");
+Console.WriteLine("Test4");
+Console.WriteLine("Test5");
